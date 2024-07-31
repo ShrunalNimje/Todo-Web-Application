42e84de03ee040600f0fbc9dd10d3a39b63d8d0e
Shrunal Nimje <shrunalnimje@gmail.com>
2024-07-31 10:29:37 +0530

    Making Todo class to Entity class

--- src/main/java/my/mood/TodoWebApplication/todo/Todo.java
+++ src/main/java/my/mood/TodoWebApplication/todo/Todo.java
@@ -2,17 +2,26 @@ package my.mood.TodoWebApplication.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Todo {
 
+	@Id
+	@GeneratedValue
 	private int id;
 	private String username;
-	
 	@Size(min = 10, message = "Please fill at least of 10 characters")
 	private String description;
 	private LocalDate targetDate;
 	private boolean done;
+	
+	public Todo() {
+		
+	}
 	
 	public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
 		this.description = description;
